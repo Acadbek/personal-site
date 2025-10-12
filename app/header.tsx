bc01737824fe8cf039255eb8b44c4b9cd995697e
asad <87940040+Acadbek@users.noreply.github.com>
2025-10-12 16:24:21 +0500

    Update header.tsx

--- app/header.tsx
+++ app/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
           className="text-zinc-600 dark:text-zinc-500"
           delay={0.5}
         >
-          Web Pentester & Frontend Developer
+          Web Pentester & Design Engineer
         </TextEffect>
       </div>
     </header>
