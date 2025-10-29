185477d3d2b93da3befc0e94d918d52163bab91f
asad <87940040+Acadbek@users.noreply.github.com>
2025-10-29 12:29:10 +0500

    Update project link for Salom UI

--- app/data.ts
+++ app/data.ts
@@ -31,7 +31,7 @@ export const PROJECTS: Project[] = [
   {
     name: 'Salom UI ~ (WIP)',
     description: 'UI library built with shadcn registry.',
-    link: '#',
+    link: 'https://salom.nosirjonov.uz',
     video: '/void.webp',
     id: 'project3',
   },
